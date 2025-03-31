2be0594768d49b65062d5f474adbcc8c2eb9337d
Aman <meamanx7@gmail.com>
2025-03-31 10:54:12 +0530

    added ball and hitter

---
+++ ball.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("red")
+        self.penup()
+
+    def move(self):
+        new_x = self.xcor() + 10
+        nww_y = self.ycor() + 10
+        self.goto(new_x,new_x)
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("hello")
+from turtle import Screen,Turtle
+from hitter import Hitter
+from  ball import Ball
+import time
+screen = Screen()
+screen.setup(width=800,height=600)
+screen.bgcolor("black")
+screen.title("pong game")
+screen.tracer(0)
+
+right_hitter = Hitter((350,0))
+left_hitter = Hitter((-350,0))
+ball = Ball()
+
+
+
+screen.listen()
+screen.onkey(right_hitter.go_up,"Up")
+screen.onkey(right_hitter.go_down,"Down")
+screen.onkey(left_hitter.go_up,"w")
+screen.onkey(left_hitter.go_down,"s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.3)
+    screen.update()
+    ball.move()
+
+
+
+
+
+
+
+
+screen.exitonclick()
